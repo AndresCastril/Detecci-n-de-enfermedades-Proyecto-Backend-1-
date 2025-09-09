3ec5bea04d6f4842daeec99378937960b9423b40
Andres54 <andrescastrillon2005@gmail.com>
2025-09-08 23:23:02 -0500

    Server y client sirven con todas lo necesario para empezar las funciones Fasta

---
+++ client/src/main/java/org/genomics/TCPClient.java
@@ -0,0 +1,75 @@
+package org.genomics;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+
+public class TCPClient {
+    private String serverAddress;
+    private int serverPort;
+
+    public TCPClient(String serverAddress, int serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+    }
+
+    public void sendMessage(String message) {
+        try {
+            // Cargar el truststore
+            String truststorePath = "client/truststore.p12";
+            String truststorePassword = "andres54";
+
+            KeyStore trustStore = KeyStore.getInstance("PKCS12");
+            try (InputStream trustStream = new FileInputStream(truststorePath)) {
+                trustStore.load(trustStream, truststorePassword.toCharArray());
+            }
+
+            // Inicializar TrustManager con el truststore
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(trustStore);
+
+            // Crear contexto SSL
+            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+
+            // Crear socket seguro
+            SSLSocketFactory factory = sslContext.getSocketFactory();
+            try (SSLSocket socket = (SSLSocket) factory.createSocket(serverAddress, serverPort)) {
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                // Enviar mensaje al servidor
+                out.println(message);
+                System.out.println("Message sent to server.");
+
+
+                String response;
+                while ((response = in.readLine()) != null) {
+                    System.out.println("Server response: " + response);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        TCPClient client = new TCPClient("localhost", 8080);
+        client.sendMessage(
+                "CREATE_PATIENT\n" +
+                        "full_name:Ana maría\n" +
+                        "document_id:104801590\n" +
+                        "age:22\n" +
+                        "sex:M\n" +
+                        "contact_email:María@example.com\n" +
+                        "registration_date:2025-09-08\n" +
+                        "clinical_notes:Test\n" +
+                        "checksum_fasta:abcdds1234\n" +
+                        "file_size_bytes:24\n" +
+                        "FASTA_FILE\n" +
+                        ">patient002\n" +
+                        "ACGTACGTACGTACGTGCGT\n" +
+                        "END_FASTA"
+        );
+    }
+}
--- server/src/main/java/org/genomics/TCPserver.java
+++ server/src/main/java/org/genomics/TCPserver.java
@@ -43,7 +43,7 @@ public class TCPServer {
                 SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                handleClient(clientSocket);
+                new Thread(() -> handleClient(clientSocket)).start();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -68,7 +68,9 @@ public class TCPServer {
             if (request.toString().startsWith("CREATE_PATIENT")) {
                 int newId = patientCounter.getAndIncrement();
                 out.println("SUCCESS");
-                out.println("patient_id:" + newId);
+                out.print("patient_id:" + newId);
+                savePatientToCSV(newId, request.toString());
+                System.out.println("Assigned patient_id: " + newId + "\n");
             } else {
                 out.println("ERROR");
                 out.println("message:Unknown command");
@@ -84,6 +86,68 @@ public class TCPServer {
         }
     }
 
+    private void savePatientToCSV(int patientId, String request) {
+        File csvFile = new File("data/patients.csv");
+        boolean fileExists = csvFile.exists();
+
+        try (FileWriter fw = new FileWriter(csvFile, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter pw = new PrintWriter(bw)) {
+
+            if (!fileExists) {
+                pw.println("patient_id,full_name,document_id,age,sex,contact_email,registration_date,clinical_notes,checksum_fasta,file_size_bytes");
+            }
+
+            String[] lines = request.split("\n");
+            String fullName = "";
+            String documentId = "";
+            String age = "";
+            String sex = "";
+            String email = "";
+            String regDate = "";
+            String notes = "";
+            String checksum = "";
+            String fileSize = "";
+
+            for (String line : lines) {
+                if (line.startsWith("full_name:")) fullName = line.split(":", 2)[1];
+                if (line.startsWith("document_id:")) documentId = line.split(":", 2)[1];
+                if (line.startsWith("age:")) age = line.split(":", 2)[1];
+                if (line.startsWith("sex:")) sex = line.split(":", 2)[1];
+                if (line.startsWith("contact_email:")) email = line.split(":", 2)[1];
+                if (line.startsWith("registration_date:")) regDate = line.split(":", 2)[1];
+                if (line.startsWith("clinical_notes:")) notes = line.split(":", 2)[1];
+                if (line.startsWith("checksum_fasta:")) checksum = line.split(":", 2)[1];
+                if (line.startsWith("file_size_bytes:")) fileSize = line.split(":", 2)[1];
+            }
+
+
+            pw.printf("%d,%s,%s,%s,%s,%s,%s,%s,%s,%d%n",
+                    patientId,
+                    escapeCsv(fullName),
+                    escapeCsv(documentId),
+                    age,
+                    sex,
+                    escapeCsv(email),
+                    escapeCsv(regDate),
+                    escapeCsv(notes),
+                    escapeCsv(checksum),
+                    fileSize);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private String escapeCsv(String field) {
+        if (field == null) return "";
+
+        if (field.contains("\"") || field.contains(",")) {
+            return "\"" + field.replace("\"", "\"\"") + "\"";
+        }
+        return field;
+    }
     public static void main(String[] args) {
         TCPServer server = new TCPServer(8080);
         server.start();
